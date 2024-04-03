3663306fc67f357b56ed69be9130e00c85f08594
beampra <s6419410001@sau.ac.th>
2024-04-03 13:09:46 +0700

    Code v3 MenuShopUI Complete

--- lib/views/home_ui.dart
+++ lib/views/home_ui.dart
@@ -1,4 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:mukrata_sau_app/views/about_ui.dart';
+import 'package:mukrata_sau_app/views/caiculate_pay_bill_ui.dart';
+import 'package:mukrata_sau_app/views/menu_shop_ui.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeUI extends StatefulWidget {
   const HomeUI({super.key});
@@ -8,8 +15,61 @@ class HomeUI extends StatefulWidget {
 }
 
 class _HomeUIState extends State<HomeUI> {
+  //สร้างตัวแปรเก็บหน้าจอ
+  List<Widget> _showUI = [
+    CalculatePayBillUI(),
+    AboutUI(),
+    MenuShopUI(),
+  ];
+  //สร้างตัวแปนกำหนดหน้าจอ
+  int _currentShowUI = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        title: Text(
+          'Tech SAU BUFFET',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: ConvexAppBar(
+        onTap: (value) {
+          setState(() {
+            _currentShowUI = value;
+          });
+        },
+        initialActiveIndex: _currentShowUI,
+        backgroundColor: Colors.deepOrange,
+        items: [
+          TabItem(
+            icon: Icon(
+              FontAwesomeIcons.moneyBill1,
+              color: Colors.pink,
+            ),
+            title: 'คิดเงิน',
+          ),
+          TabItem(
+            icon: Icon(
+              FontAwesomeIcons.home,
+              color: Colors.pink
+            ),
+            title: 'Home',
+          ),
+          TabItem(
+            icon: Icon(
+              FontAwesomeIcons.star,
+              color: Colors.pink,
+            ),
+            title: 'เกี่ยวกับ',
+          ),
+        ],
+      ),
+      body: _showUI[_currentShowUI],
+    );
   }
 }
--- lib/views/menu_shop_ui.dart
+++ lib/views/menu_shop_ui.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_final_fields
+
 import 'package:flutter/material.dart';
 
 class MenuShopUI extends StatefulWidget {
@@ -8,8 +10,108 @@ class MenuShopUI extends StatefulWidget {
 }
 
 class _MenuShopUIState extends State<MenuShopUI> {
+  //สร้างตัวแปรเก็บรายการแสดงในlistviewของเมนูเด็ด
+  List<String> _showmanu = [
+    'assets/images/img1.png',
+    'assets/images/img2.png',
+    'assets/images/img3.png',
+    'assets/images/img4.png',
+    'assets/images/img5.png',
+    'assets/images/img6.png',
+  ];
+  //ร้านในเครือ
+  List<String> _showshop = [
+    'assets/images/shop1.png',
+    'assets/images/shop2.png',
+    'assets/images/shop3.png',
+    'assets/images/shop4.png',
+    'assets/images/shop5.png',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 240, 198, 212),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: MediaQuery.of(context).size.width * 0.7,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            Text(
+              'เมนูเด็ด',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            Container(
+              height: 100.0,
+              padding: EdgeInsets.only(
+                left: 30.0,
+                right: 30.0,
+              ),
+              child: ListView.builder(
+                itemCount: _showmanu.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      left: 10.0,
+                      right: 10.0,
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Image.asset(
+                        _showmanu[index],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            Text(
+              'ร้านในเครือ',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _showshop.length,
+                scrollDirection: Axis.vertical,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      left: 70.0,
+                      right: 70.0,
+                      top: 15.0,
+                      bottom: 8.0,
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Image.asset(
+                        _showshop[index],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
